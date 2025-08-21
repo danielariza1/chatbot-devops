f8f8c6825f3d54585aa592f564270d63cff85337
danielariza1 <deimosgame08@gmail.com>
2025-08-21 11:00:55 -0500

    Prueba pipeline con Dockerhub

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 def chatbot(mensaje):
     if "hola" in mensaje.lower():
-        return "👋 Hola, soy tu ChatBot DevOps!"
+        return "👋 Hola, soy tu ChatBot Pepe DevOps!"
     return "No entendí tu mensaje."
 
 if __name__ == "__main__":
